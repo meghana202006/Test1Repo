01535eda0a39319a393b0e8e6cd99c8a7ceb3b4e
meghana202006 <meghanarodd20@gmail.com>
2025-04-25 14:45:15 +0530

    Create JavaPro.java

---
+++ JavaPro.java
@@ -0,0 +1,8 @@
+public class JavaPro{
+
+  public static void main(String args[])
+  {
+      System.out.println("Welcome to Java Project!");
+      System.out.println("Java test file in Github");
+  }
+}
